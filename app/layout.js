2b497d9858a1ebbff4528c186995b1217533ae12
Raghav2125 <94511249+Raghav2125@users.noreply.github.com>
2025-05-14 12:20:11 +0530

    Update layout.js with addition of Footer

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer/Footers"
 
 import { Poppins } from 'next/font/google';
 
@@ -35,6 +36,7 @@ export default function RootLayout({ children }) {
 
         <Navbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
