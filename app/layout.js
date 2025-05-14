8fd0b7fe8c9caba40157c97ed158ce88ecc4599a
Raghav2125 <94511249+Raghav2125@users.noreply.github.com>
2025-05-14 12:21:22 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
-import Footer from "@/components/Footer/Footers"
+import Footer from "@/components/Footer/Footers";
 
 import { Poppins } from 'next/font/google';
 
