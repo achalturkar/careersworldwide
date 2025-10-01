f262815f896ce74d52ab24da58f8cf158e2c6b46
Achal <achalyturkar@gmail.com>
2025-10-01 16:42:37 +0530

    SEO redirect 301

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    async redirects() {
+    return [
+      {
+        source: '/36203/01/smt-endmill-price-in-johor', 
+        
+        destination: '/', 
+        
+        // This makes it a 301 Permanent Redirect (CRITICAL for SEO)
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
